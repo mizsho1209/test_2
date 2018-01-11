b27a949c70267964a80ef57f28247e19070a7f49
mizsho <mimizsho@gmail.com>
2018-01-11 22:20:16 +0900

    オフィス終わり

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,50 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all.order(created_at: 'desc')
   end
 
   def show
     @post = Post.find(params[:id])
+
   end
 
   def new
+    @post=Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+  def create
+    if  @post = Post.new(post_params)
+      @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+        @post.visited = 0
+        redirect_to post_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 end
+
+private
+      def post_params
+        params.require(:post).permit(:title, :body)
+      end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+# config.action_mailer.default_url_options = { host: '0.0.0.0:3000' }
+
 # Initialize the Rails application.
 Rails.application.initialize!
